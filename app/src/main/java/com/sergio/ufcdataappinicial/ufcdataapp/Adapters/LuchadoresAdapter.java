f8bc98ff2b1d3f4e984995cbc9ebf4955a00a51b
Sergio Sánchez <sergio.sanchez.corrionero@gmail.com>
2018-04-12 00:10:23 +0200

    ·Muestreo de imagenes y nombres de todos los luchadores (beta)

--- app/src/main/java/com/sergio/ufcdataappinicial/ufcdataapp/Adapters/LuchadoresAdapter.java
+++ app/src/main/java/com/sergio/ufcdataappinicial/ufcdataapp/Adapters/LuchadoresAdapter.java
@@ -5,19 +5,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.sergio.ufcdataappinicial.ufcdataapp.Model.Luchador;
 import com.sergio.ufcdataappinicial.ufcdataapp.R;
+import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LuchadoresAdapter extends RecyclerView.Adapter<LuchadoresAdapter.LuchadoresViewHolder> {
 
     private Context context;
     private Luchador[] luchadores;
-
-
 
     public LuchadoresAdapter(Context context, Luchador[] luchadores) {
         this.context = context;
@@ -52,17 +53,25 @@ public class LuchadoresAdapter extends RecyclerView.Adapter<LuchadoresAdapter.Lu
 
     public static class LuchadoresViewHolder extends RecyclerView.ViewHolder {
 
+        Context context;
+
         private TextView txt;
+        private ImageView img;
 
         public LuchadoresViewHolder(Context context, View itemView) {
             super(itemView);
 
             txt = itemView.findViewById(R.id.txtLuchador1);
+            img = itemView.findViewById(R.id.img);
+
+            this.context = context;
         }
 
         public void bindLuchador(Luchador luchador) {
             txt.setText(luchador.getNombre() + " " +luchador.getApellido());
-
+            if (luchador.getImgPerfil() != null) {
+                Picasso.with(context).load(luchador.getImgPerfil()).into(img);
+            }
         }
     }
 }
